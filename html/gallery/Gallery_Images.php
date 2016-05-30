26a9ca54746c9f2a924ef1ae3ea100cb4763d3a3
BuddhiWathsala <b.wathsala.bw@gmail.com>
2016-05-31 01:24:47 +0530

    event gallery basics

---
+++ html/gallery/Gallery_Images.php
@@ -0,0 +1,5 @@
+<?php include("../includes/main_header.php");?>
+<div class=image>
+	 <object  width="50%" height="300px" data="linux.jpg"></object> 
+</div>
+<?php include("../includes/main_footer.php");?>
