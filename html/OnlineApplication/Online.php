4cf8c8b820b7509c9a25b9d10c14389cbd021487
dilani138 <dilanishyamali1589906@gmail.com>
2016-06-09 13:22:08 -0700

    dilani's commit

---
+++ html/OnlineApplication/Online.php
@@ -0,0 +1,153 @@
+<!DOCTYPE HTML >
+
+<html lang= "en-US" >
+<head>
+<!-- *Beginning of the web page -->
+<!-- ....................................................................................................................... -->
+<!-- Title of the web page -->
+<!-- ....................................................................................................................... -->
+	<title> </title>
+<!-- ....................................................................................................................... -->
+
+	
+<!-- Links and meta data -->
+<!-- ....................................................................................................................... -->
+	<link rel= "stylesheet" type= "TEXT/CSS" href= "../css/structure.css" />
+	<link rel= "stylesheet" type= "TEXT/CSS" href= "../css/navigation.css" />
+<!-- ....................................................................................................................... -->
+<div class= "container" >
+
+<!-- Header of the web page -->
+<!-- ....................................................................................................................... -->
+	<div class= "header" >
+		<img class= "banner" src= "../Images/banner/vtasl_banner.png" alt= "Vocational Training Authority of Sri Lanka" />
+	</div>
+<!-- ....................................................................................................................... -->
+
+	
+<!-- Navigation of the web page -->
+<!-- ....................................................................................................................... -->
+	<div class= "topnav" >
+		<ul>
+			<li> <a href= "home.html" class= "active" > Home </a> </li>
+			
+			<li class= "dropdown"> 
+				<a href= "about_us.html" class= "dropbtn" > About Us </a> 
+				 	<div class= "dropdown_content" >
+						<a href= "who_we_are.html" > Who We Are </a>
+						<a href= "authorities_message.html" > Authorities Message </a>
+					</div>
+			</li>
+			
+			<li class= "dropdown" >
+				<a href= "gallery.html" > Gallery </a> 
+					<div class= "dropdown_content" >
+						<a href= "gallery_img.html" > Photos </a>
+						<a href= "gallery_vid.html" > Videos </a>
+					</div>
+			</li>
+			
+			<li> <a href= "skill_directory.html" > Skill Directory </a> </li>
+			
+			<li> <a href= "apply_on-line.html" > Apply On-line </a></li>
+			
+			<li> <a href= "contact,html" > Contact </a> </li>
+			
+			<li> <a href= "faqs.html" > FAQs </a> </li>
+			
+			<li class= "right" > <a href= "profiles.html" > Profiles </a> </li>
+			
+			<li class= "right" > <a href= "search.html" > Search </a> </li>
+			
+		</ul>
+	</div>
+<!-- ....................................................................................................................... -->
+
+	
+<!-- Content of the web page -->	
+<!-- ....................................................................................................................... -->
+	<div class= "content" >
+		
+	
+	
+	
+	
+	
+	
+	
+	</div>
+<!-- ....................................................................................................................... -->
+
+	
+
+
+</head>
+<body>
+
+<p><br>
+<br/>
+APPLICATION FORM<p/>
+<form>
+
+<fieldset><legend>Personal</legend>
+<label for="field1"><span>Full Name: <span class="required">*  </span></span><input type="text" class="input-field" name="field1" value="" /></label><br><br/>
+<label for="field2"><span>Address: <span class="required">*  </span></span><input type="text" class="input-field" <textarea name="field2" rows="5" cols="30" value=""></textarea></label><br><br/>
+<label for="field3"><span>Date of birth:  <span class="required">*  </span></span><input type="date" class="input-field" name="field3" value="" /></label><br><br/>
+<label for="field4"><span>Email: <span class="required">*  </span></span><input type="email" class="input-field" name="field2" value="" /></label><br><br/>
+<label for="field5"><span>Phone: <span class="required">*  </span></span><input type="text" class="input-field" name="field3" value="" /></label><br><br/>
+<label for="field6"><span>Course</span><select name="field4" class="select-field"><br><br/>
+<option Course_value="Appointment">Appointment</option>
+<option Course_value="Interview">Interview</option>
+<option Course_value="Regarding a post">Regarding a post</option>
+<label for="field7"><span>Branch: </span><select name="field3" class="select-field"><br><br/>
+<option value="Galle">Galle</option>
+<option value="Mathara">Mathara</option>
+<option value="Ambalangoda">Ambalangoda</option>
+<!--<label for="field6"><span>Message <span class="required">*</span></span><textarea name="field6" class="textarea-field"></textarea></label>-->
+<label for="fieldset"><span>&nbsp;</span><input type="submit" value="Submit" /></label><br><br/>
+</fieldset>
+</form>
+<br>
+<br/>
+
+
+<!-- Bottom of the web page -->	
+<!-- ....................................................................................................................... -->
+	<div class= "hr1" > </div>
+	<div class= "ft_leave" > </div>
+	
+	<div class= "ft_contact" >
+		<h4>Contact Us </h4>
+		<p>
+		Vocational Training Authority<br/>
+		Mailing address:<br/>
+		354/2 , "Nipunatha Piyasa" ,<br/>
+		Elvitigala Mawatha,<br/>
+		Colombo 05,<br/>
+		Sri Lanka.<br/><br/>
+
+		Telephone : +94112581904<br/>
+		Hot line : +94117270270<br/>
+		Fax : +94112581904<br/>
+		Email :info@vtasl.gov.lk<br/> 
+		</p> 
+	</div>
+		
+	<div class= "ft_links" > </div>
+	<div class= "ft_posts" > </div>
+	<div class= "ft_courses" > </div>
+<!-- ....................................................................................................................... -->
+
+
+<!-- Footer of the web page -->
+<!-- ....................................................................................................................... -->
+	<div class= "footer">  © Copyright Vocational Training Authority 2014. </div>
+<!-- ....................................................................................................................... -->
+
+
+</div>
+
+<!-- End of web page -->
+<!-- ....................................................................................................................... -->
+</body>
+</html>
