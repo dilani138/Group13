71aefcaa596febed0c273e2fe75c05adcfdd218f
dilani138 <dilanishyamali1589906@gmail.com>
2016-06-11 19:21:46 -0700

    dilani

---
+++ html/OnlineApplication/Result/Result.php
@@ -0,0 +1,69 @@
+<html>
+	<head>
+	<title>
+			Result
+	</title>
+		<style>
+			table, th, td {
+				border: 0px solid black;
+
+		</style>
+	</head>
+<body>
+
+<br></br>
+<table cellspacing="10" align="center">
+
+<!--district.center,course........................................................................................................-->
+  <tr>
+    <th><img src = "district.jpg" align ="left" ></th>
+	<th><img src = "center.jpg" align ="center" ></th>
+	<th><img src = "course.jpg" align ="right" ></th>
+  </tr>
+  
+  
+  <tr>
+  
+  <!--district......................................................................................................................-->
+    <td><span class="required">  </span><select name="field4" align ="left" bgcolor="#ffcc99" class="select-field"><br><br/>
+																<option Course_value="Galle">###################</option>
+																<option Course_value="Galle">Galle</option>
+																<option Course_value="Mathara">Mathara</option>
+																<option Course_value="Colombo">Colombo</option></td>
+																
+	<!--center..........................................................................................................................-->															
+    <td><span class="required">  </span><select name="field4" align ="center" class="select-field"><br><br/>
+																<option Course_value="#####">###################</option>
+																<option Course_value="ddddd">ddddd</option>
+																
+	<!--course...........................................................................................................................-->															
+	<td><span class="required">  </span><select name="field4" align ="right"class="select-field"><br><br/>
+																<option Course_value="Galle">###################</option>
+																<option Course_value="Appointment">Appointment</option>
+																<option Course_value="Interview">Interview</option>
+																<option Course_value="Regarding a post">Regarding a post</option></td>
+
+<!--search................................................................................................................................-->																
+	<td><img src = "search.jpg" align ="right" type="search" id="mySearch"></td>	
+
+	<tr>
+		<br><th><img src = "result.jpg" align ="left" ></th>
+	</tr>
+</table>
+  </tr>
+  <tr>
+		<th>
+			<table border ="0"
+				   width ="56%"
+				   align ="center" >
+				<tr>
+					<td><textarea cols="80" rows="25"></textarea></td>
+				</tr>
+			</table>
+		</th>
+	</tr>
+
+
+
+</body>
+</html>
