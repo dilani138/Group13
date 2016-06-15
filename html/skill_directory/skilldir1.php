fab7a9de1a142381bf5d7a9697db552f0a6d046d
subodhika <janani.subodhika930@gmail.com>
2016-06-14 21:58:43 -0700

    create student interface

---
+++ html/skill_directory/skilldir1.php
@@ -0,0 +1,25 @@
+<?php include("../includes/new_main_header.php");?>
+
+<div style="background-color:blue; color:black; padding:15px; ">
+
+<h1 style="text-align:center;">student name</h1>
+<img src="linux.jpg"  style="width:100px;height:100px;">
+
+
+</div>
+<div style="background-color:green; color:black; padding:15px; ">
+<button type="button">Profile</button>
+<button type="button">Vacancy</button>
+<button type="button">Search</button>
+<button type="button">Logout</button>
+
+
+
+</div>
+
+
+
+</body>
+
+
+</html>
