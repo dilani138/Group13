81ffa71e80b3407a26920df0675d100224d2573d
Dinesh Gamage <ndmsgamage@gmail.com>
2016-06-19 12:18:25 +0530

    Check this

---
+++ New/php/contact_page.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html >
+
+<html lang="en-US">
+
+<head>
+	
+	<title> Contact_Page </title>
+
+	<link rel="stylesheet" href="../css/media_queries_1.css" />
+	<link rel="stylesheet" href="../basic_structure.css" />
+
+
+</head>
+<body>
+		
+<div class="contianer">
+	<div class="header"> 
+
+	</div>
+
+	<div class="content">
+		<div class="col-b-2">
+			<div class="col-c-1">
+				
+				<h1> Contact Details </h1>
+	            <table border= "0" class="tb-1" >
+
+	                <tr>
+	                    <td style="vertical-align: top;">  Mailing address: </td>
+	                    <td style="vertical-align: top;"> 
+	                    	 354/2 , "Nipunatha Piyasa" ,<br/>
+	                          Elvitigala Mawatha, <br/>
+	                          Colombo 05, <br/>
+	                          Sri Lanka. <br/>	                         
+	                    </td>
+	                </tr>
+
+	                <tr>
+	                    <td> Hot line : </td>
+	                    <td> +94117270270</td>
+	                </tr>
+
+	                <tr>
+	                   <td> Telephone : </td>
+	                   <td> +94112596517-16 </td>
+	               	</tr>
+
+	                <tr>
+	                    <td> Fax : </td>
+	                    <td>+94112502342 </td>
+	                </tr>
+
+	                <tr>
+	                    <td> Email : </td>
+	                    <td> info@vtasl.gov.lk </td>
+	                </tr>
+
+	                <tr>
+	                    <td> Web : </td>
+	                    <td> www.vtasl.gov.lk </td>
+	                </tr>
+	                
+	            </table>
+			</div>		
+		</div>
+
+		<div class="col-b-2-r right">
+			<div class="col-c-1">
+				
+				<h1> Satellite Map </h1>
+
+				<img src="../images/map.png" alt="Satellite Map" border="2px" class="" />
+
+			</div>
+		</div>
+
+		<div class="col-b-2">
+			<div class="col-c-1">
+				
+				<h1> Leave Your Message </h1>
+				<form method="post">
+
+					<table border="0" class="tb-1" >
+						
+						<tr>
+							<td> Name:</td>
+							<td> <input type="text" name="name" value="" /></td>
+						</tr>
+
+						<tr>
+							<td> Email:</td>
+							<td> <input type="text" name="email" value="" /></td>
+						</tr>
+
+						<tr>
+							<td> Telephone:</td>
+							<td> <input type="text" name="telephone" value="" /></td>
+						</tr>
+
+						<tr>
+							<td> Subject:</td>
+							<td> <input type="text" name="subject" value="" /></td>
+						</tr>
+
+						<tr>
+							<td> Message:</td>
+							<td> <textarea name="message" > </textarea></td>
+						</tr>
+
+						<tr>
+						<td></td>
+		                	<td > <input type="submit" name="Send Message" value="Send Message" /></td>
+		                </tr>
+
+					</table>	
+				</form>
+
+			</div>			
+		</div>
+
+
+
+
+
+
+
+
+
+	</div>
+
+</div>
+
+
+</body>
+</html>
---
+++ New/php/nav_active_home.php
@@ -0,0 +1,95 @@
+<!--
+================================================================================================
+	* |ACTIVE HOME NAVIGATION PAGE. 
+	* |LINK TO HOME PAGE.
+================================================================================================
+-->
+
+
+<!DOCTYPE html >
+<html lang= "en-US" >
+
+<head>
+	
+	<title> Responsive Navigation Only </title>
+	
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="../css/media_queries_1.css" /> <!--MEDIA QUERY STYLE SHEET -->
+	<link rel="stylesheet" type="text/css" href="../css/responsive_navigation.css" />  <!--NAVIGATION STYLE SHEET -->
+	<link rel="stylesheet" type="text/css" href="../css/basic_structure.css" />
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css"> <!--FONT FA FA STYLE SHEET -->
+
+	
+</head>
+<body>
+
+
+
+	<div class= "col-b-3 topnav" > <!-- RESPONSIVE FOR DIFFERENT SCREEN SIZES -->
+	
+		<label for="show-menu" class="show-menu">Navigation</label>  <!-- NAVIGATION BUTTON ON MOBILE AND TABLET SCREEN -->
+		
+		<input type="checkbox" id="show-menu" role="button">	
+		
+			<ul id="menu" >  <!-- NAVIGATION BEGIN HERE -->
+			
+			
+				<li> <a href="" class= "active" > Home </a> </li> <!-- HOME PAGE -->
+				
+				
+				<li>																			
+				<a href="" > About Us <i class= "fa fa-angle-double-down" style= "color: #ffffff;"> </i> </a>  <!-- ABOUT US PAGE -->
+				
+					<ul class="hidden">
+						<li> <a href="about_us.html" > Who We Are </a> </li>
+						<li> <a href="authorities_message.html" > Authorities Message </a> </li>
+					</ul>
+
+				</li>
+				
+				
+				<li>
+				<a href="#"> Gallery <i class= "fa fa-angle-double-down" style= "color: #ffffff;"> </i> </a> <!-- GALLERY PAGE -->
+				
+					<ul class="hidden">
+						<li> <a href="gal_photos.html" > Photos </a> </li>
+						<li> <a href="gal_videos.html" > Videos </a> </li>
+					</ul>
+
+				</li>
+				
+				<li> <a href="skill_diectory.html"> Skill Directory </a> </li><!-- SKILL DIRECTORY PAGE -->
+			
+				<li> <a href="search.html"> <i class= "fa fa-search fa-lg " style= "color: #ffffff;"></i>  Search  </a> </li> <!-- SEARCH PAGE -->
+				
+				<li> <a href="apply_on-line.html"> Apply On-line </a> </li> <!-- APPLY ON LINE PAGE -->
+				
+				<li> <a href="contact.html"> Contact </a> </li><!-- CONTACT PAGE -->
+				
+				<li> <a href="loging_page.html"> <i class= "fa fa-user fa-lg " style= "color: #ffffff;"></i>  Profiles </a> </li> <!-- PROFILES PAGE -->
+				
+				<li> <a href="faqs.html"> FAQs </a> </li> <!-- FAQS PAGE -->
+				
+				
+				<li>
+				<a href="#"> Follow Us <i class= "fa fa-angle-double-down" style= "color: #ffffff;"> </i> </a> <!-- FOLLOW US BUTTON -->
+				
+					<ul class="hidden">
+					 <!-- GOOGLE+ -->
+						<li class= "right"> <a href="https://plus.google.com/u/0/118127281917719301738/posts">  <i class= "fa fa-google-plus-square fa-lg" style= "color: #000000;"></i>Google+ </a> </li> 
+					 <!-- YOU TUBE -->
+						<li class= "right"> <a href="https://www.youtube.com/channel/UCQXQr6J27of68k7wPyqdJpg">  <i class= "fa fa-youtube-square fa-lg" style= "color: #000000;"></i> You Tube</a> </li>
+					 <!--FACE BOOK -->
+						<li class= "right"> <a href="https://www.facebook.com/ictvta"> <i class= "fa fa-facebook-square fa-lg" style= "color: #000000;"> </i> FaceBook </a> </li>
+					</ul>
+
+				</li>
+		</ul> <!-- NAVIGATION ENDS HERE -->
+	
+	</div>
+
+
+</body>
+</html>
