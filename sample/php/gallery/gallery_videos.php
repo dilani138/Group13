2c53de8d35175b9852ca7f825eda3be2cbdc9841
Dinesh Gamage <ndmsgamage@gmail.com>
2016-06-21 23:51:17 +0530

    Here I have created the sample folder structure. In these structure I have included complete header, navigations and two pages in home directory. Also there are some incomplete pages, but we have to edit only the content of those files. Also content of the footer should change with further developments. PLEASE DONT EDIT THESE FILES WITHIN THIS sample FOLDER

---
+++ sample/php/gallery/gallery_videos.php
@@ -0,0 +1,16 @@
+<!-- |THIS IS gallery videos PAGE| -->
+
+<!-- |OVERALL HEADER OF THE PAGE| -->
+<?php include("../../includes/main/header.php") ?>  <!-- |HEADER OF THE PAGE| -->
+<?php include("../../includes/navigations/nav_active_gallery.php") ?> <!-- |NAVIGATION OF THE PAGE| -->
+   
+<!-- |CONTENT OF THE PAGE| -->
+   
+<div class="contianer">
+    |
+	|Gallery-videos
+	|
+</div>
+
+<!-- |FOOTER OF THE PAGE| -->
+<?php include("../../includes/main/footer.php") ?>
---
+++ sample/php/search/apply_on_line.php
@@ -0,0 +1,17 @@
+<!-- |THIS IS apply on line PAGE| -->
+
+<!-- |OVERALL HEADER OF THE PAGE| -->
+<?php include("../../includes/main/header.php") ?>  <!-- |HEADER OF THE PAGE| -->
+<?php include("../../includes/navigations/nav_active_apply_on_line.php") ?> <!-- |NAVIGATION OF THE PAGE| -->
+   
+<!-- |CONTENT OF THE PAGE| -->
+   
+<div class="contianer">
+    |
+	|This is On line application
+	|
+	
+</div>
+
+<!-- |FOOTER OF THE PAGE| -->
+<?php include("../../includes/main/footer.php") ?>
