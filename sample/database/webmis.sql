0e6a44fcdc57591146e6be526fe30fee7087929f
Dinesh Gamage <ndmsgamage@gmail.com>
2016-08-01 15:41:04 +0530

    database

---
+++ sample/database/webmis.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2016 at 12:08 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webmis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appliers`
+--
+
+CREATE TABLE `appliers` (
+  `applier_id` int(11) NOT NULL,
+  `initials` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `nic` varchar(12) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` tinyint(4) NOT NULL,
+  `address_01` varchar(50) NOT NULL,
+  `address_02` varchar(50) DEFAULT NULL,
+  `city` varchar(20) NOT NULL,
+  `district` varchar(20) NOT NULL,
+  `home_telephone` int(12) NOT NULL,
+  `mobile` int(12) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `time` tinyint(1) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `center`
+--
+
+CREATE TABLE `center` (
+  `center_id` int(11) NOT NULL,
+  `center_name` varchar(20) NOT NULL,
+  `district` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `company_id` int(11) NOT NULL,
+  `user_name` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `registration_date` date NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `telephone_01` int(12) NOT NULL,
+  `telephone_02` int(12) DEFAULT NULL,
+  `address` varchar(50) NOT NULL,
+  `fax` int(12) NOT NULL,
+  `details` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(30) NOT NULL,
+  `time_period` varchar(50) NOT NULL,
+  `nvq_level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_center`
+--
+
+CREATE TABLE `course_center` (
+  `center_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `graduates`
+--
+
+CREATE TABLE `graduates` (
+  `student_id` int(11) NOT NULL,
+  `user_name` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `telephone` int(12) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `extra_qualification` varchar(255) NOT NULL,
+  `gpa` decimal(5,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vacancy`
+--
+
+CREATE TABLE `vacancy` (
+  `vacancy_id` int(11) NOT NULL,
+  `vacancy_name` varchar(50) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `details` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appliers`
+--
+ALTER TABLE `appliers`
+  ADD PRIMARY KEY (`applier_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `center`
+--
+ALTER TABLE `center`
+  ADD PRIMARY KEY (`center_id`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `course_center`
+--
+ALTER TABLE `course_center`
+  ADD PRIMARY KEY (`center_id`,`course_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `graduates`
+--
+ALTER TABLE `graduates`
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `vacancy`
+--
+ALTER TABLE `vacancy`
+  ADD PRIMARY KEY (`vacancy_id`),
+  ADD KEY `company_id` (`company_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appliers`
+--
+ALTER TABLE `appliers`
+  MODIFY `applier_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `center`
+--
+ALTER TABLE `center`
+  MODIFY `center_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `graduates`
+--
+ALTER TABLE `graduates`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `vacancy`
+--
+ALTER TABLE `vacancy`
+  MODIFY `vacancy_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appliers`
+--
+ALTER TABLE `appliers`
+  ADD CONSTRAINT `appliers_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  ADD CONSTRAINT `appliers_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  ADD CONSTRAINT `appliers_ibfk_3` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`);
+
+--
+-- Constraints for table `course_center`
+--
+ALTER TABLE `course_center`
+  ADD CONSTRAINT `course_center_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  ADD CONSTRAINT `course_center_ibfk_2` FOREIGN KEY (`center_id`) REFERENCES `center` (`center_id`);
+
+--
+-- Constraints for table `graduates`
+--
+ALTER TABLE `graduates`
+  ADD CONSTRAINT `graduates_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  ADD CONSTRAINT `graduates_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  ADD CONSTRAINT `graduates_ibfk_3` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`);
+
+--
+-- Constraints for table `vacancy`
+--
+ALTER TABLE `vacancy`
+  ADD CONSTRAINT `vacancy_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
