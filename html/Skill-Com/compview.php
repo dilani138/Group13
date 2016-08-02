4c03658fd375ce8c62be02c9a5eb38aee6df9dc4
zuha_23 <zuha203@gmail.com>
2016-08-02 00:46:14 -0400

    change skill directory company view

---
+++ html/Skill-Com/compview.php
@@ -0,0 +1,156 @@
+
+<!DOCTYPE HTML >
+
+<html lang= "en-US" >
+
+<head >
+
+	<title> Skill Directory-Company Vaccancy </title>
+	
+</head>
+
+<body>
+<div class= "container" >
+
+
+		<!DOCTYPE HTML >
+
+<html lang= "en-US" >
+
+<head>
+
+<!-- Links and meta data -->
+	<link rel= "stylesheet" type= "TEXT/CSS" href= "../css/structure.css" />
+	
+
+<!-- Title of the web page -->
+	<title> Header Of The Web Page </title>
+
+</head>
+
+<!-- ............................................................................................................................... -->
+
+<!-- Header of the web page -->
+<div class= "container_spec" >
+	<div class= "header" >
+		<img class= "banner" src= "../images/banner/vtasl_banner.png" alt= "Vocational Training Authority of Sri Lanka" />
+	</div>
+</div>
+<!-- ............................................................................................................................... -->
+	
+</head>
+<body>
+</body>
+</html><!DOCTYPE HTML >
+
+<html lang= "en-US" >
+
+<head>
+
+	<title> Navigation Of The Web Page </title>
+	
+<!-- Links and meta data -->
+
+	<link rel= "stylesheet" type= "TEXT/CSS" href= "../css/new_nav.css" />
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+</head>
+
+<!-- Navigation of the web page -->
+<div class= "container_spec" >
+	<div class= "topnav" >
+		<ul>
+			<li> <a href="home.html" > Home </a> </li>
+			
+			<li class= "dropdown" >
+				<a href="#" class="dropbtn"> About Us </a>
+					<div class= "dropdown-content" >
+						<a href="about_us.html" > Who We Are </a>
+						<a href="authorities_message.html" > Authorities Message </a>
+					</div>
+			</li>
+			
+			<li class= "dropdown" >
+				<a href="#" class="dropbtn"> Gallery </a>
+					<div class= "dropdown-content" >
+						<a href="gal_photos.html" > Photos </a>
+						<a href="gal_videos.html" > Videos </a>
+					</div>
+			</li>
+			
+			<li> <a href="skill_diectory.html"> Skill Directory </a> </li>
+			
+			<li> <a class="active" href="search.html"> <i class= "fa fa-search fa-lg " style= "color: #ffffff;"></i>  Search  </a> </li>
+			
+			<li> <a href="apply_on-line.html"> Apply On-line </a> </li>
+			
+			<li> <a href="contact.html"> Contact </a> </li>
+			
+			<li> <a href="loging_page.html"> <i class= "fa fa-user fa-lg " style= "color: #ffffff;"></i>  Profiles </a> </li>
+			
+			<li> <a href="faqs.html"> FAQs </a> </li>
+			
+			<li class= "right"> <a href="https://plus.google.com/u/0/118127281917719301738/posts">  <i class= "fa fa-google-plus-square fa-lg" style= "color: #ffffff;"></i> </a> </li>
+			
+			<li class= "right"> <a href="https://www.youtube.com/channel/UCQXQr6J27of68k7wPyqdJpg">  <i class= "fa fa-youtube-square fa-lg" style= "color: #ffffff;"></i> </a> </li>
+			
+			<li class= "right"> <a href="https://www.facebook.com/ictvta"> <i class= "fa fa-facebook-square fa-lg" style= "color: #ffffff;"> </i> </a> </li>
+			
+		</ul>
+	</div>
+</div>	
+	<style>
+.button {
+    background-color:#000000;
+    text-align: center;
+	padding: 5px 5px;
+	color: white;
+	cursor: pointer;
+	margin: 10px 12px;
+}
+.static {
+    position: static;
+    border: 1px solid #000000;
+	background-color:#B0E0E6;
+	
+}
+</style>
+</head>
+
+<body>
+<!-- profile picture -->
+<div style="background-color:#87CEEB; padding:25px">
+<img style="float:left" src="user.png" width:"150" height:"150"><h3>Abc Co ltd</h3><br><br><br><br><br>
+</div>
+<!-- black row with buttons -->
+<div class="button">
+<button class="button" type="button">Profile</button>
+<button class="button" type="button">Vacancies</button>
+<button class="button" type="button">Search</button>
+<button class="button" type="button">Logout</button>
+</div>
+
+<!-- input text area -->
+<div class="static">
+<form action=_self name=post>
+Title: <br></tr><tr><input type="text" name="vaccancy" size="100%" style="background-color:#6495ED"><br>
+Description: <br><input type="text" name="vaccancy" size="100%"style="background-color:	#6495ED"><br>
+<input type="submit" value="Post"></form>
+</div>
+</body>
+</html>
+
+</head>
+<body>
+</body>
+</html>
+
+
+	
+	
+</div>
+
+</body>
+
+</html>
+
